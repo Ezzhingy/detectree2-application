1f2545a0f2e8fd3fc8ce2dd329923e65edf97a76
Eugene Zhang <zhang.eug@gmail.com>
2025-03-10 01:03:58 -0400

    detectree2 lib setup

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,39 @@
 from flask import Flask
 
+from detectree2.preprocessing.tiling import tile_data
+from detectree2.models.outputs import project_to_geojson, stitch_crowns, clean_crowns
+from detectree2.models.predict import predict_on_data
+from detectree2.models.train import setup_cfg
+from detectron2.engine import DefaultPredictor
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    # Path to site folder and orthomosaic
+    site_path = "./detectree2"
+    img_path = site_path + "/INSERT_IMAGE_PATH_HERE.tif"
+    tiles_path = site_path + "/tilespred/"
+
+    # Specify tiling
+    buffer = 30
+    tile_width = 40
+    tile_height = 40
+    tile_data(img_path, tiles_path, buffer, tile_width, tile_height, dtype_bool = True)
+
+    trained_model = "./230103_randresize_full.pth"
+    cfg = setup_cfg(update_model=trained_model)
+    predict_on_data(tiles_path, predictor=DefaultPredictor(cfg))
+
+    project_to_geojson(tiles_path, tiles_path + "predictions/", tiles_path + "predictions_geo/")
+
+    crowns = stitch_crowns(tiles_path + "predictions_geo/", 1)
+    clean = clean_crowns(crowns, 0.6, confidence=0) # set a confidence>0 to filter out less confident crowns
+
+    clean = clean[clean["Confidence_score"] > 0.5] # step included for illustration - can be done in clean_crowns func
+
+    clean = clean.set_geometry(clean.simplify(0.3))
+
+    clean.to_file(site_path + "/crowns_out.gpkg")
+
     return "hello world"
